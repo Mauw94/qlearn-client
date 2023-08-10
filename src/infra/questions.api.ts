7f5f54c14271b938aa6450329c5069d4cfbc5630
Maurits <mauritsseelen@hotmail.com>
2023-08-10 13:53:46 +0200

    updated api endpoint

--- src/infra/questions.api.ts
+++ src/infra/questions.api.ts
@@ -2,7 +2,7 @@ import type { Difficulty } from "../domain/enum/difficulty.enum";
 
 export const BaseUrl = () => {
     console.log(process.env.NODE_ENV);
-    return process.env.NODE_ENV === 'production' ? 'https://ambitious-beach-0ec311103.3.azurestaticapps.net/question/' : 'http://localhost:3000/question/';
+    return process.env.NODE_ENV === 'production' ? 'https://qlearn-api.azurewebsites.net/question/' : 'http://localhost:3000/question/';
 }
 export const InitCache = (difficulty: Difficulty, clientId: string) => { return `init/${difficulty}/${clientId}` }
 export const fetchAll: string = "all/";

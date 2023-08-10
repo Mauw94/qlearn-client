17f85c2c73922760bd41710ee91436b8af98d254
Maurits <mauritsseelen@hotmail.com>
2023-08-10 17:34:31 +0200

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,9 +7,14 @@
 	<h1>About this app</h1>
 
 	<p>
-		This is a <a href="/">QLearn</a>ing app to study all kinds of topics. For now though,
-		only basic math questions are implemented.
+		This is a <a href="/">QLearn</a>ing app to study all kinds of topics. For now though, only basic
+		arithmetic operation questions are implemented.
 	</p>
 
 	<pre>QLearn</pre>
+
+	<p>
+		The questioned arithmetic operations will be further be improved upon. Then other areas of study
+		will be implemented.
+	</p>
 </div>
